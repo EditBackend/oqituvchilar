17744b9975451117ac8bde5a74cf3a20c003eb18
EditBackend <commanderx002@gmail.com>
2026-01-07 12:16:22 +0300

    CORS added for localhost and render

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Oqituvchi, TestStudent, OqituvchiTahrirlash, OqituvchiSMS,FirstTeacher, Guruh
+from .serializers import FirstTeacherSerializer
 
-# Register your models here.
+admin.site.register(Oqituvchi)
+admin.site.register(TestStudent)
+admin.site.register(OqituvchiTahrirlash)
+admin.site.register(OqituvchiSMS)
+admin.site.register(FirstTeacher)
+admin.site.register(Guruh)
