23e6224c9e9616e27b523581a6ab28531fdec705
EditBackend <commanderx002@gmail.com>
2026-01-07 13:06:13 +0300

    Added admin creation and setup

--- oqituvchilar/urls.py
+++ oqituvchilar/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0)),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0)),
